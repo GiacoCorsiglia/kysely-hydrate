294543d0045b2e006d0281c29ba95dbb1f51141b
Giaco Corsiglia <GiacoCorsiglia@gmail.com>
2025-12-29 11:35:37 -0700

    feat: export error classes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export { createHydrator, type Hydrator, hydrateData } from "./hydrator.ts";
 export { hydrate } from "./query-builder.ts";
 export { HydratePlugin } from "./plugin.ts";
 export { map } from "./mapped-expression.ts";
+export * from "./helpers/errors.ts";
